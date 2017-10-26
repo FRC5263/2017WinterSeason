ede922c9daa2bbaa1e23374fa9790d830f0425ce
EGeiss <EGeiss@users.noreply.github.com>
2017-10-26 13:21:39 -0400

    Create TeleOperated.java

---
+++ src/org/usfirst/frc/team5263/robot/TeleOperated.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5263.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class TeleOperated {
+  //define varibles up here
+  
+  //make meat and potatos
+    public TeleOperated() {
+	  }
+    
+    public void init(String mode){
+      
+    }
+    
+    public void Periodic() {
+      
+    }
+}
