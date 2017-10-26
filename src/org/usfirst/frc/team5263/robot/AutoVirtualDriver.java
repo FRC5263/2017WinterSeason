97870928bc4d346e52128a660390d84086ccc399
EGeiss <EGeiss@users.noreply.github.com>
2017-10-26 13:27:14 -0400

    Created AutoVirtualDriver.java

---
+++ src/org/usfirst/frc/team5263/robot/AutoVirtualDriver.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team5263.robot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class AutoVirtualDriver {
+  
+    public AutoVirtualDriver (){
+      
+    }
+    
+    public void init() {
+      
+    }
+    
+    public void periodicAuto() {
+    
+    }
+    
+    
+}
